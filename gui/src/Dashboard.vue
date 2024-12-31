407e78c64f086c985dd25a4c0c3a8afbe40481c2
Lv Yonghuan <1914128278@qq.com>
2024-12-31 23:19:19 +0800

    filter

--- gui/src/Dashboard.vue
+++ gui/src/Dashboard.vue
@@ -1,71 +1,69 @@
 <template>
+  <el-container>
+    <!-- 侧边栏 -->
+    <el-aside width="200px" class="aside">
+      <el-select v-model="selectedOption" placeholder="请选择网卡">
+        <el-option 
+          v-for="item in optionList" 
+          :key="item.name"
+          :label="item.description"
+          :value="item.name">
+        </el-option>
+      </el-select>
+
       <el-container>
-        <!-- 侧边栏 -->
-          <el-aside width="200px" class="aside">
-            <el-select v-model="selectedOption" placeholder="请选择网卡">
-              <el-option 
-                v-for="item in optionList" 
-                :key="item.name"
-                :label="item.description"
-                :value="item.name">
-              </el-option>
-            </el-select>
+        <el-row class="button-group" justify="center" style="margin-top: 20px">
+          <el-button 
+            type="success" 
+            class="custom-button" 
+            style="margin: 5px" 
+            :disabled="isCapturing" 
+            @click="startCapture">开始捕获</el-button>
+          <el-button 
+            type="warning" 
+            class="custom-button" 
+            style="margin: 5px" 
+            :disabled="!isCapturing" 
+            @click="restartCapture">重新捕获</el-button>
+          <el-button 
+            type="danger" 
+            class="custom-button" 
+            style="margin: 5px" 
+            :disabled="!isCapturing" 
+            @click="stopCapture">停止捕获</el-button>
+        </el-row>
+      </el-container>
+    </el-aside>
 
-            <el-container>
-              <el-row class="button-group" justify="center" style="margin-top: 20px">
-                <el-button 
-                  type="success" 
-                  class="custom-button" 
-                  style="margin: 5px" 
-                  :disabled="isCapturing" 
-                  @click="startCapture">开始捕获</el-button>
-                <el-button 
-                  type="warning" 
-                  class="custom-button" 
-                  style="margin: 5px" 
-                  :disabled="!isCapturing" 
-                  @click="restartCapture">重新捕获</el-button>
-                <el-button 
-                  type="danger" 
-                  class="custom-button" 
-                  style="margin: 5px" 
-                  :disabled="!isCapturing" 
-                  @click="stopCapture">停止捕获</el-button>
-              </el-row>
-            </el-container>
+    <!-- 主体内容 -->
+    <el-container>
+      <!-- 顶部过滤器 -->
+      <el-header style="background-color: #f5f7fa">
+        <el-row :gutter="20" style="margin-top: 10px">
+          <el-col :span="18">
+            <el-input placeholder="请输入过滤信息......" v-model="searchText"></el-input>
+          </el-col>
+          <el-col :span="6">
+            <el-button type="primary" @click="applyFilter">启用</el-button>
+          </el-col>
+        </el-row>
+      </el-header>
 
-          </el-aside>
-
-
-      <!-- 主体内容 -->
-      <el-container>
-        <!-- 顶部过滤器 -->
-        <el-header style="background-color: #f5f7fa">
-          <el-row :gutter="20" style="margin-top: 10px">
-            <el-col :span="18">
-              <el-input placeholder="请输入过滤信息......" v-model="searchText"></el-input>
-            </el-col>
-            <el-col :span="6">
-              <el-button type="primary">启用</el-button>
-            </el-col>
-          </el-row>
-        </el-header>
-
-        <!-- 信息接收区 -->
-        <el-main>
-          <Sniffer 
-        :selectedOption="selectedOption"
-        :searchText="searchText"
-        :isCapturing="isCapturing"
+      <!-- 信息接收区 -->
+      <el-main>
+        <Sniffer 
+          :selectedOption="selectedOption"
+          :searchText="searchText"
+          :isCapturing="isCapturing"
         />
-        </el-main>
-      </el-container>
-        
-      </el-container>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-import Sniffer from './Sniffer.vue'
+import axios from 'axios';
+import Sniffer from './Sniffer.vue';
 
 export default {
   components: {
@@ -76,7 +74,6 @@ export default {
       selectedOption: '',
       searchText: '',
       optionList: [],
-
       isCapturing: false
     }
   },
@@ -89,6 +86,20 @@ export default {
     },
     stopCapture() {
       this.isCapturing = false;
+    },
+    applyFilter() {
+      const filter = this.searchText;
+      axios.get(`http://127.0.0.1:8080/setFilter`, {
+        params: {
+          filter: filter
+        }
+      })
+      .then(response => {
+        console.log('Filter applied:', response.data);
+      })
+      .catch(error => {
+        console.error('Error applying filter:', error);
+      });
     }
   },
   props: {
@@ -117,7 +128,7 @@ export default {
 }
 
 .custom-button {
-        width: 100px;
-        color: white;
-      }
+  width: 100px;
+  color: white;
+}
 </style>
